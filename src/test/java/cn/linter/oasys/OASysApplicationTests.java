ee39b688d8fa952e7eac86ad25905a9705748bd0
wangxiaoyang <15847424609@163.com>
2020-08-24 10:05:46 +0800

    修改ApplicationTests访问权限

--- src/test/java/cn/linter/oasys/OASysApplicationTests.java
+++ src/test/java/cn/linter/oasys/OASysApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class OASysApplicationTests {
+public class OASysApplicationTests {
 
     @Test
     public void contextLoads() {
