568b5093f48e7b42724cde0b5ac0c1fa1e6ff979
wangxiaoyang <15847424609@163.com>
2020-12-19 13:42:21 +0800

    perf: 获取公钥返回值类型改为JSONObject

--- oasys-auth/src/main/java/cn/linter/oasys/auth/controller/KeyController.java
+++ oasys-auth/src/main/java/cn/linter/oasys/auth/controller/KeyController.java
@@ -2,13 +2,13 @@ package cn.linter.oasys.auth.controller;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.KeyPair;
 import java.security.interfaces.RSAPublicKey;
-import java.util.Map;
 
 /**
  * RSA密钥控制器
@@ -19,19 +19,11 @@ import java.util.Map;
 @RestController
 public class KeyController {
 
-    /**
-     * RSA密钥对
-     */
     @Autowired
     private KeyPair keyPair;
 
-    /**
-     * 获取RSA公钥
-     *
-     * @return RSA公钥
-     */
     @GetMapping("/key/public")
-    public Map<String, Object> getPublicKey() {
+    public JSONObject getPublicKey() {
         RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
         RSAKey key = new RSAKey.Builder(publicKey).build();
         return new JWKSet(key).toJSONObject();
