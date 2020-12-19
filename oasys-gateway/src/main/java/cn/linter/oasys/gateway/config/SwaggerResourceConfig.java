534bfcb3136d7f4d6912568c68bbcb5a84622f63
wangxiaoyang <15847424609@163.com>
2020-12-19 13:34:23 +0800

    feat: 新增Swagger资源排除配置

--- oasys-gateway/src/main/java/cn/linter/oasys/gateway/config/SwaggerResourceConfig.java
+++ oasys-gateway/src/main/java/cn/linter/oasys/gateway/config/SwaggerResourceConfig.java
@@ -1,8 +1,8 @@
 package cn.linter.oasys.gateway.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.config.GatewayProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.support.NameUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,7 +11,6 @@ import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author wangxiaoyang
@@ -22,27 +21,28 @@ import java.util.stream.Collectors;
 public class SwaggerResourceConfig implements SwaggerResourcesProvider {
 
     @Autowired
-    private RouteLocator routeLocator;
-    @Autowired
     private GatewayProperties gatewayProperties;
+
+    private final String LOCATION_KEY = NameUtils.GENERATED_NAME_PREFIX + "0";
+
+    @Value("#{'${swagger.resources.exclude}'.split(',')}")
+    private List<String> excludeList;
 
     @Override
     public List<SwaggerResource> get() {
         List<SwaggerResource> resources = new ArrayList<>();
-        List<String> routes = new ArrayList<>();
-        routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
-        gatewayProperties.getRoutes().stream().filter(routeDefinition -> routes.contains(routeDefinition.getId())).forEach(route -> {
-            route.getPredicates().stream().filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName())).forEach(predicateDefinition ->
-                    resources.add(swaggerResource(route.getId(), predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0").replace("**", "v2/api-docs"))));
-        });
-        return resources.stream().filter(resource->!resource.getName().startsWith("auth")).collect(Collectors.toList());
+        gatewayProperties.getRoutes().stream().filter(route -> !excludeList.contains(route.getId()))
+                .forEach(route -> route.getPredicates().stream().filter(predicate -> ("Path").equals(predicate.getName()))
+                        .forEach(predicate -> resources.add(newSwaggerResource(route.getId(), predicate.getArgs().get(LOCATION_KEY)))));
+        return resources;
     }
 
-    private SwaggerResource swaggerResource(String name, String location) {
+    private SwaggerResource newSwaggerResource(String name, String location) {
         SwaggerResource swaggerResource = new SwaggerResource();
         swaggerResource.setName(name);
-        swaggerResource.setLocation(location);
         swaggerResource.setSwaggerVersion("1.0");
+        swaggerResource.setLocation(location.replace("**", "v2/api-docs"));
         return swaggerResource;
     }
+
 }
