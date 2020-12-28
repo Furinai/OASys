c9f32e1af754745deea9ec6fc704a1e8f4a9b656
wangxiaoyang <15847424609@163.com>
2020-12-28 21:50:43 +0800

    perf: 移除冗余方法

--- oasys-common/src/main/java/cn/linter/oasys/common/utils/JwtUtil.java
+++ oasys-common/src/main/java/cn/linter/oasys/common/utils/JwtUtil.java
@@ -1,8 +1,6 @@
 package cn.linter.oasys.common.utils;
 
-import com.nimbusds.jose.*;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jose.JWSObject;
 
 import java.text.ParseException;
 
@@ -18,47 +16,6 @@ public class JwtUtil {
      * Token前缀
      */
     public static final String TOKEN_PREFIX = "Bearer ";
-
-    public String generate(String payloadStr, String secret) throws JOSEException {
-        //创建JWS头，设置签名算法和类型
-        JWSHeader jwsHeader = new JWSHeader.Builder(JWSAlgorithm.HS256).
-                type(JOSEObjectType.JWT)
-                .build();
-        //将负载信息封装到Payload中
-        Payload payload = new Payload(payloadStr);
-        //创建JWS对象
-        JWSObject jwsObject = new JWSObject(jwsHeader, payload);
-        //创建HMAC签名器
-        JWSSigner jwsSigner = new MACSigner(secret);
-        //签名
-        jwsObject.sign(jwsSigner);
-        return jwsObject.serialize();
-    }
-
-    /**
-     * 从Token获取用户ID
-     *
-     * @param token Token
-     * @return 用户ID
-     */
-    public static Long getUserId(String token) {
-        JWSObject jwsObject = null;
-        try {
-            jwsObject = JWSObject.parse(token.replace(TOKEN_PREFIX, ""));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return jwsObject != null ? Long.valueOf((String) jwsObject.getPayload().toJSONObject().get("user_id")) : null;
-    }
-
-    public boolean verify(String token, String secret) throws ParseException, JOSEException {
-        //从token中解析JWS对象
-        JWSObject jwsObject = JWSObject.parse(token);
-        //创建HMAC验证器
-        JWSVerifier jwsVerifier = new MACVerifier(secret);
-
-        return jwsObject.verify(jwsVerifier);
-    }
 
     /**
      * 从Token获取用户名
