4552491977f45b48c9bb6fa82da3cacb26a1f685
wangxiaoyang <15847424609@163.com>
2021-01-04 23:26:46 +0800

    feat: 新增用户进入和离开聊天室的提示

--- oasys-service/oasys-chat/src/main/java/cn/linter/oasys/chat/handler/ChatWebSocketHandler.java
+++ oasys-service/oasys-chat/src/main/java/cn/linter/oasys/chat/handler/ChatWebSocketHandler.java
@@ -9,6 +9,8 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.Map;
+
 
 /**
  * 聊天WebSocket处理器
@@ -29,6 +31,9 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         SessionContainer.add(session);
+        Map<String, Object> attributes = session.getAttributes();
+        String content = attributes.get("fullName") + "进入了聊天室";
+        messagePublisher.publish(attributes, Type.SYSTEM, content);
     }
 
     @Override
@@ -39,6 +44,9 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
         SessionContainer.remove(session);
+        Map<String, Object> attributes = session.getAttributes();
+        String content = attributes.get("fullName") + "离开了聊天室";
+        messagePublisher.publish(attributes, Type.SYSTEM, content);
     }
 
 }
