ceb6d9522b93cc6b309890769a0c603287030600
wangxiaoyang <15847424609@163.com>
2021-03-24 18:12:15 +0800

    refactor: 移除冗余@EnableFeignClients注解

--- oasys-gateway/src/main/java/cn/linter/oasys/gateway/GatewayApplication.java
+++ oasys-gateway/src/main/java/cn/linter/oasys/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package cn.linter.oasys.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 网关启动类
@@ -13,7 +12,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class GatewayApplication {
 
     public static void main(String[] args) {
